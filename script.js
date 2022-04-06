b29de8abfdb918ef33523d5ba9b78d719ac82d2e
Hariharnautiyal <101192066+Hariharnautiyal@users.noreply.github.com>
2022-04-06 10:05:49 +0530

    update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Basketball player", "Hacker", "Coder", "Designer"],
+        strings: ["Basketball player", "Designer", "Coder", "AppEncoder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Basketball player", "Hacker", "Coder", "Designer"],
+        strings: ["Basketball player", "Designer", "Coder", "AppEncoder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
